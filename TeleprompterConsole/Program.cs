b92ce664990dca77aa406c7786d6ee2e62dadeeb
yojiro <ged@nifty.com>
2017-08-10 16:21:14 +0900

    基本形。

--- TeleprompterConsole/Program.cs
+++ TeleprompterConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,23 @@ namespace TeleprompterConsole
     {
         static void Main(string[] args)
         {
+            var lines = ReadFrom("C:/Users/ged/documents/visual studio 2017/Projects/TeleprompterConsole/TeleprompterConsole/sampleQuotes.txt");
+            foreach (var line in lines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+
+        static IEnumerable<string> ReadFrom(string file)
+        {
+            string line;
+            using (var reader = File.OpenText(file))
+            {
+                while ((line = reader.ReadLine()) != null)
+                {
+                    yield return line;
+                }
+            }
         }
     }
 }
---
+++ TeleprompterConsole/Readme.md
@@ -0,0 +1,3 @@
+﻿# This project
+
+Tried this sample.=>[Console Application \| Microsoft Docs](https://docs.microsoft.com/en-us/dotnet/csharp/tutorials/console-teleprompter)
