70c95f015f059b064a9f0e32a4686d5b4cd54646
yojiro <ged@nifty.com>
2017-08-10 17:06:23 +0900

    IEnumerableをListに転換

--- TeleprompterConsole/Program.cs
+++ TeleprompterConsole/Program.cs
@@ -12,10 +12,26 @@ namespace TeleprompterConsole
         static void Main(string[] args)
         {
             var lines = ReadFrom("C:/Users/ged/documents/visual studio 2017/Projects/TeleprompterConsole/TeleprompterConsole/sampleQuotes.txt");
-            foreach (var line in lines)
+
+
+            //IEnumerableをListに変換。
+            var lines2 = new List<string>();
+            lines2 = lines.ToList();
+            lines2.Add("This is the end of lines2.");// 行を足せる！
+
+            for (var i = 0; i < 10; i++)
+                Console.WriteLine($"{i}     " + lines2[i]);
+            Console.WriteLine("Enter Key please.");
+            Console.ReadLine();
+
+
+            foreach (var line in lines2)
             {
                 Console.WriteLine(line);
             }
+
+            Console.WriteLine("Enter Key please.");
+            Console.ReadLine();
         }
 
         static IEnumerable<string> ReadFrom(string file)
